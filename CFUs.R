442082c6679de9d3ac898cf47a73ebb02dad7479
Catherine Gubert <cgubert@umich.edu>
2014-12-10 16:51:47 -0500

    Added README.Rmd and some other stuffs

---
+++ CFUs.R
@@ -0,0 +1,15 @@
+#Want a program that will calculate CFUs in original (stock) bacterial culture
+#when given number of colonies on plate and dilution factor
+#so, function should run off of something like "CFU(colonies=250,DF=10:1)"
+#also include funtion that will give appropriate dilution factor to use to 
+#count colonies from a solution with a given CFU concentration
+#so this function would look something like "DF(CFU=15.0*10^8)
+#Dilution factor (DF): ratio of final volume/aliquot volume 
+#(final volume = aliquot + diluent)
+
+#Should be able to calculate CFUs from a data set/list of colony count values
+#Input data from colony counts I did last week to use 
+
+CFU<-function(colonies,DF){
+  
+}
