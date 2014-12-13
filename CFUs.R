9fec72989e3167b87c28e08ff863ee6d2333fa39
Catherine Gubert <cgubert@umich.edu>
2014-12-13 09:41:00 -0500

    getting an outline of functions

--- CFUs.R
+++ CFUs.R
@@ -1,9 +1,11 @@
-#Want a program that will calculate CFUs in original (stock) bacterial culture
+#volumes input as mL quantities (i.e. 1.0=1mL, 0.900=900µL)
+#want a program that will calculate CFUs in original (stock) bacterial culture
 #when given number of colonies on plate and dilution factor
 #so, function should run off of something like "CFU(colonies=250,DF=10:1)"
 #also include funtion that will give appropriate dilution factor to use to 
 #count colonies from a solution with a given CFU concentration
 #so this function would look something like "DF(CFU=15.0*10^8)
+
 #Dilution factor (DF): ratio of final volume/aliquot volume 
 #(final volume = aliquot + diluent)
 #have users input dilution factors as ratio format (ex. 1:10)
@@ -15,11 +17,17 @@
 final.vols<-rep(.500,8)
 dilutent.vols<-rep(.450,8)
 aliquot.vols<-rep(0.05,8)
+#load user data instead of sample data
+final.vols<-("your.final.vols")
+dilutent.vols<-("your.dilutent.vols")
+aliquot.vols<-("your.aliquot.vols")
+#do useful things with the data
 dilution.factor<-function(final.vols,aliquot.vols,dilutent.vols){
-  if (final.vols,aliquot.vols){final.vols/aliquot.vols}
-  else if (aliquot.vols,dilutent.vols){(aliquot.vols+dilutent.vols)/aliquot.vols}
-  
-  
+  if (final.vols,aliquot.vols){DFs<-final.vols/aliquot.vols}
+  else if (aliquot.vols,dilutent.vols){DFs<-(aliquot.vols+dilutent.vols)/aliquot.vols}
+  else if (final.vols,dilutent.vols){DFs<-final.vols/(final.vols-dilutent.vols)}
+  else(//please input at least two the the required aliquot.vols, dilutent.vols, or final.vols vectors)
+  return(DFs)
 }
 
 #get user data into acceptable format
